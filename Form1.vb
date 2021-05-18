32e7eb2d6670b442a2deee4213736637106a065d
abobkerelaghel <abobkerelaghel@gmail.com>
2021-05-18 22:14:00 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    '
+    ' Primary Color: #2A3035
+    '
+    Dim PrimaryColor As Integer = "#2A3035"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = ColorTranslator.FromHtml(PrimaryColor)
+    End Sub
+End Class
