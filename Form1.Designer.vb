b7874b99223504bbcce1b6877f550ba1b73f1c1e
abobkerelaghel <abobkerelaghel@gmail.com>
2021-05-19 13:01:12 +0200

    almost done with the sign up form

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class SignInUpForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,18 +22,289 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SignInUpForm))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.firstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.startHereLabel = New System.Windows.Forms.Label()
+        Me.CreateAnAccountWithUsLabel = New System.Windows.Forms.Label()
+        Me.alreadyAMemberLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.firstNameLabel = New System.Windows.Forms.Label()
+        Me.lastNameLabel = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Form1
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(691, 728)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 31.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(188, 555)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(297, 126)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Never Miss" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "A Contact"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(54, 50)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(570, 486)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'firstNameTextBox
+        '
+        Me.firstNameTextBox.BackColor = System.Drawing.Color.White
+        Me.firstNameTextBox.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.firstNameTextBox.Location = New System.Drawing.Point(765, 170)
+        Me.firstNameTextBox.Name = "firstNameTextBox"
+        Me.firstNameTextBox.Size = New System.Drawing.Size(369, 27)
+        Me.firstNameTextBox.TabIndex = 2
+        '
+        'startHereLabel
+        '
+        Me.startHereLabel.AutoSize = True
+        Me.startHereLabel.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.startHereLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.startHereLabel.Location = New System.Drawing.Point(739, 26)
+        Me.startHereLabel.Name = "startHereLabel"
+        Me.startHereLabel.Size = New System.Drawing.Size(86, 23)
+        Me.startHereLabel.TabIndex = 8
+        Me.startHereLabel.Text = "Start Here"
+        '
+        'CreateAnAccountWithUsLabel
+        '
+        Me.CreateAnAccountWithUsLabel.AutoSize = True
+        Me.CreateAnAccountWithUsLabel.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.CreateAnAccountWithUsLabel.ForeColor = System.Drawing.Color.White
+        Me.CreateAnAccountWithUsLabel.Location = New System.Drawing.Point(724, 49)
+        Me.CreateAnAccountWithUsLabel.Name = "CreateAnAccountWithUsLabel"
+        Me.CreateAnAccountWithUsLabel.Size = New System.Drawing.Size(411, 46)
+        Me.CreateAnAccountWithUsLabel.TabIndex = 9
+        Me.CreateAnAccountWithUsLabel.Text = "Create an Account with us"
+        '
+        'alreadyAMemberLabel
+        '
+        Me.alreadyAMemberLabel.AutoSize = True
+        Me.alreadyAMemberLabel.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.alreadyAMemberLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.alreadyAMemberLabel.Location = New System.Drawing.Point(739, 107)
+        Me.alreadyAMemberLabel.Name = "alreadyAMemberLabel"
+        Me.alreadyAMemberLabel.Size = New System.Drawing.Size(158, 23)
+        Me.alreadyAMemberLabel.TabIndex = 10
+        Me.alreadyAMemberLabel.Text = "Already a member?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(42, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(53, Byte), Integer))
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(894, 107)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 23)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Log in"
+        '
+        'firstNameLabel
+        '
+        Me.firstNameLabel.AutoSize = True
+        Me.firstNameLabel.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.firstNameLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.firstNameLabel.Location = New System.Drawing.Point(754, 144)
+        Me.firstNameLabel.Name = "firstNameLabel"
+        Me.firstNameLabel.Size = New System.Drawing.Size(92, 23)
+        Me.firstNameLabel.TabIndex = 12
+        Me.firstNameLabel.Text = "First Name"
+        '
+        'lastNameLabel
+        '
+        Me.lastNameLabel.AutoSize = True
+        Me.lastNameLabel.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lastNameLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.lastNameLabel.Location = New System.Drawing.Point(754, 231)
+        Me.lastNameLabel.Name = "lastNameLabel"
+        Me.lastNameLabel.Size = New System.Drawing.Size(91, 23)
+        Me.lastNameLabel.TabIndex = 14
+        Me.lastNameLabel.Text = "Last Name"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(765, 257)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(369, 27)
+        Me.TextBox1.TabIndex = 13
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(755, 329)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 23)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Last Name"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.White
+        Me.TextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.TextBox2.Location = New System.Drawing.Point(766, 355)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(369, 27)
+        Me.TextBox2.TabIndex = 15
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(755, 427)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 23)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "Last Name"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.White
+        Me.TextBox3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.TextBox3.Location = New System.Drawing.Point(766, 453)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(369, 27)
+        Me.TextBox3.TabIndex = 17
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.Label5.Location = New System.Drawing.Point(755, 520)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(91, 23)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Last Name"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.White
+        Me.TextBox4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.TextBox4.Location = New System.Drawing.Point(766, 546)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(369, 27)
+        Me.TextBox4.TabIndex = 19
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.Label6.Location = New System.Drawing.Point(755, 608)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(91, 23)
+        Me.Label6.TabIndex = 22
+        Me.Label6.Text = "Last Name"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.White
+        Me.TextBox5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(82, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.TextBox5.Location = New System.Drawing.Point(766, 634)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(369, 27)
+        Me.TextBox5.TabIndex = 21
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(885, 677)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(123, 43)
+        Me.Button1.TabIndex = 23
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'SignInUpForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(42, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(53, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(1304, 627)
-        Me.Name = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1178, 752)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.lastNameLabel)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.firstNameLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.alreadyAMemberLabel)
+        Me.Controls.Add(Me.CreateAnAccountWithUsLabel)
+        Me.Controls.Add(Me.startHereLabel)
+        Me.Controls.Add(Me.firstNameTextBox)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "SignInUpForm"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents firstNameTextBox As TextBox
+    Friend WithEvents startHereLabel As Label
+    Friend WithEvents CreateAnAccountWithUsLabel As Label
+    Friend WithEvents alreadyAMemberLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents firstNameLabel As Label
+    Friend WithEvents lastNameLabel As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,10 +1,55 @@
-﻿Public Class Form1
+﻿Public Class SignInUpForm
     '
     ' Primary Color: #2A3035
     '
-    Dim PrimaryColor As Integer = "#2A3035"
+    Dim PrimaryColor As String = "#2A3035"
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.BackColor = ColorTranslator.FromHtml(PrimaryColor)
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles firstNameTextBox.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        firstNameTextBox.Hide()
+        MsgBox(firstNameTextBox.Visible)
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles startHereLabel.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles alreadyAMemberLabel.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles CreateAnAccountWithUsLabel.Click
+
+    End Sub
+
+    Private Sub Label2_Click_1(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click_1(sender As Object, e As EventArgs) Handles firstNameLabel.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged_1(sender As Object, e As EventArgs)
+
     End Sub
 End Class
